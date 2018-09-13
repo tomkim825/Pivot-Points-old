7a78bf690124a4bb0a937a197c7cf2857a408360
tomkim825 <tk825@hotmail.com>
2018-09-13 01:14:17 -0700

    initial upload

---
+++ src/Title.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Title extends Component {
+
+render() {
+  return (
+    <div className="">
+    <br/> 
+    - 
+    <br/>  <br/>
+       R3:  
+       <br/>  <br/>
+       R2: 
+       <br/>  <br/>
+       R1: 
+       <br/>  <br/>
+       Pivot: 
+       <br/>  <br/>
+       S1: 
+       <br/>  <br/>
+       S2: 
+       <br/>  <br/>
+       S3: 
+    </div>
+  );
+}
+}
+
+export default Title;
